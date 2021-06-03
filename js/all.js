1659c2f3c48717d3c85f717eadf53425f824026c
yu19941994 <yishian.yu@gmail.com>
2021-06-03 20:26:29 +0800

    fix to object structure

--- js/all.js
+++ js/all.js
@@ -1,56 +1,105 @@
 const loginBtn = document.getElementById('loginBtn')
-const checkBtn = document.getElementById('checkBtn')
 const addProductBtn = document.getElementById('addProductBtn')
-const deleteProductBtn = document.getElementById('deleteProductBtn')
-
-// const fileInput = document.querySelector('#file')
+const productList = document.getElementById('productList')
+const productCount = document.getElementById('productCount')
 
 const url = `https://vue3-course-api.hexschool.io`
 const path = `uy_neish`
-const token = document.cookie.replace(/(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/, "$1")
-console.log(token)
-axios.defaults.headers.common['Authorization'] = token
 
+loginBtn.addEventListener('click', login)
+
+// 取得 Token
+// 將 Token 存到 Cookie
 function login (e) {
   const username = document.getElementById('username').value
   const password = document.getElementById('password').value
+  const data = {
+    username,
+    password
+  }
   e.preventDefault()
   console.log(username, password)
-  axios.post(`${url}/admin/signin`,
-  {
-    'username': username,
-    'password': password
-  })
+  axios.post(`${url}/admin/signin`, data)
     .then(res => {
       console.log(res)
-      const {token, expired} = res.data
-      console.log(token, expired)
-      document.cookie = `myToken = ${token}; expires = ${new Date(expired)}`
-      window.location = `product.html`
-      // getProduct()
+      if(res.data.success === true) {
+        const {token, expired} = res.data
+        console.log(token, expired)
+        document.cookie = `myToken = ${token}; expires = ${new Date(expired)}`
+        // window.location = `product.html`
+        app.init()
+      }else{
+        alert('帳號或密碼輸入有誤喲！')
+      }
     })
     .catch(err => {console.log(err)})
-  // console.log('hi')
 }
 
-function checkLogin (e) {
-  e.preventDefault()
-  axios.post(`${url}/api/user/check`)
-    .then(res => console.log(res))
+const app = {
+  data: {
+    product: [],
+    content: ''
+    // productId: ''
+  },
+  init () {
+    // 將 Cookie 取出來
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/, "$1")
+    console.log(token)
+    axios.defaults.headers.common['Authorization'] = token
+    this.getProduct()
+  },
+  getProduct () {
+    axios.get(`${url}/api/${path}/admin/products`)
+      .then(res => {
+        console.log(res)
+        if(res.data.success === true) {
+          this.data.product = res.data.products
+          console.log(this.data.product) 
+          this.renderProduct(this.data.product)
+        }
+      })
+      .catch(err => console.log(err))
+  },
+  renderProduct (product) {
+    this.data.content = ''
+    product.forEach(item => {
+      this.data.content+=`
+        <tr>
+          <td>${item.title}</td>
+          <td width="120">
+            ${item.origin_price}
+          </td>
+          <td width="120">
+            ${item.price}
+          </td>
+          <td width="100">
+            <span class="">${item.is_enabled}</span>
+          </td>
+          <td width="120">
+            <button type="button" class="btn btn-sm btn-outline-danger move deleteBtn" data-action="remove"
+              data-id="${item.id}"> 刪除 </button>
+          </td>
+        </tr>
+      `
+      productList.innerHTML = this.data.content
+    })
+    productCount.innerHTML = this.data.product.length
+    productList.addEventListener('click', app.deleteSingleProduct)
+  },
+  deleteSingleProduct (e) {
+    if(e.target.nodeName === 'BUTTON'){
+      console.log(e.target.dataset.id)
+      const productId = e.target.dataset.id
+      axios.delete(`${url}/api/${path}/admin/product/${productId}`)
+        .then(res => {
+          console.log(res)
+          if(res.data.success === true) {
+            app.getProduct()
+          }
+        })
+        .catch(err => console.log(err))
+    }
+  }
 }
 
-// function fileUpload () {
-//   console.log('upload')
-//   console.dir(fileInput.files[0])
-//   const file = fileInput.files[0]
-//   const formData = new FormData()
-//   formData.append('file-to-upload', file)
-//   axios.post(`${url}/api/${path}/admin/upload`, formData)
-//     .then(res => {
-//       console.log(res)
-//     })
-// }
 
-loginBtn.addEventListener('click', login)
-checkBtn.addEventListener('click', checkLogin)
-// fileInput.addEventListener('change', fileUpload)
